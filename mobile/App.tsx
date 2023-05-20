864a84e062f100ac6977b9f3070cced6c1f2f939
Guilherme Toledo <totoledao@hotmail.com>
2023-05-19 22:51:25 -0300

    Added fonts mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,24 @@
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!fontsLoaded) return null
+
   return (
-    <View className=" flex-1 p-10 bg-zinc-950 items-center justify-center">
-      <Text className="text-zinc-50 font-bold text-5xl">Hello World</Text>
+    <View className=" flex-1 items-center justify-center bg-gray-900">
+      <Text className="font-title text-5xl text-gray-50">Hello World</Text>
       <StatusBar style="light" translucent />
     </View>
   )
