ea88d6531f5c26d3ce20e7c71fd3220c361c2c19
Guilherme Toledo <totoledao@hotmail.com>
2023-05-19 23:45:19 -0300

    Home Mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,7 +5,11 @@ import {
   useFonts,
 } from '@expo-google-fonts/roboto'
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+
+import blurBg from './src/assets/bg-blur.png'
+import LogoLine from './src/assets/logoLine'
+import Stripes from './src/assets/stripes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,9 +21,41 @@ export default function App() {
   if (!fontsLoaded) return null
 
   return (
-    <View className=" flex-1 items-center justify-center bg-gray-900">
-      <Text className="font-title text-5xl text-gray-50">Hello World</Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 justify-center bg-gray-900 px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
       <StatusBar style="light" translucent />
-    </View>
+
+      <Stripes />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <LogoLine />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.8}
+          className="rounded-full bg-green-500 px-5 py-3"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+    </ImageBackground>
   )
 }
