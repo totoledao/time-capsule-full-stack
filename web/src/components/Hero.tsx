5f0e80930b9fd3fd28c95f5a4705c00db48bf56b
Guilherme Toledo <totoledao@hotmail.com>
2023-05-28 12:36:01 -0300

    Correction route

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -15,7 +15,7 @@ export function Hero() {
           com o mundo!
         </p>
         <a
-          href="./memories/new"
+          href="/memories/new"
           className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
         >
           CADASTRAR LEMBRANÇA
