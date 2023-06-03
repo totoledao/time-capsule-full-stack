c146afa85e4f87255b40c357f0d6ba047ae30d50
Guilherme Toledo <totoledao@hotmail.com>
2023-06-03 11:34:12 -0300

    Implemented middleware and protected routes

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -6,14 +6,17 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
 
+  // Check for redirection cookie
+  const redirectTo = request.cookies.get('redirectTo')?.value
+
   // Get the token from back-end using the code
   const registerResponse = await api.post('register', {
     code,
   })
   const { token } = registerResponse.data
 
-  // Redirect to Home and save the token in a cookie
-  const redirectURL = new URL('/', request.url)
+  // Redirect to URL or Home and save the token in a cookie
+  const redirectURL = redirectTo ?? new URL('/', request.url)
   const cookieExpirationInSeconds = 60 * 60 * 24 * 30 // 1 month
 
   return NextResponse.redirect(redirectURL, {
