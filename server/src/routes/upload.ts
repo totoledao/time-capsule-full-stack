6f72539febe28973117198414a11f5287039db77
Guilherme Toledo <totoledao@hotmail.com>
2023-06-17 11:48:44 -0300

    Correction check for max file size upload

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { randomUUID } from "node:crypto";
-import { createWriteStream } from "node:fs";
+import { createWriteStream, unlinkSync } from "node:fs";
 import { extname, resolve } from "node:path";
 import { pipeline } from "node:stream";
 import { promisify } from "node:util";
@@ -20,15 +20,18 @@ export async function uploadRoutes(app: FastifyInstance) {
     });
 
     if (!upload) {
-      return response.status(400).send({ error: "Must upload a file" });
+      return response
+        .status(400)
+        .send({ success: false, error: "Must upload a file" });
     }
+
     const mimetypeRegex = /^(image|video)\/[a-zA-Z0-9]+/;
     const isValidFileFormart = mimetypeRegex.test(upload.mimetype);
 
     if (!isValidFileFormart) {
       return response
         .status(400)
-        .send({ error: "File must be a image or video" });
+        .send({ success: false, error: "File must be an image or video" });
     }
 
     const fileID = randomUUID();
@@ -40,6 +43,13 @@ export async function uploadRoutes(app: FastifyInstance) {
     );
 
     await pump(upload.file, writeStream);
+
+    if (upload.file.truncated) {
+      unlinkSync(resolve(__dirname, "../../uploads/", fileName));
+      return response
+        .status(400)
+        .send({ success: false, error: "File must be smaller than 5MB" });
+    }
 
     const hostUrl = request.protocol.concat("://").concat(request.hostname);
     const fileUrl = new URL(`/uploads/${fileName}`, hostUrl).toString();
