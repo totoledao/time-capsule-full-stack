3b4b4a0b04ccef5e712287a64fe34139a3efc4e0
Guilherme Toledo <totoledao@hotmail.com>
2023-06-30 14:39:23 -0300

    Added view Public Memories in Home

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -41,11 +41,50 @@ const FilePreview = ({ mediaURL }: { mediaURL: string }) => {
   )
 }
 
+const RenderMemories = ({
+  memories,
+  isPublic = false,
+}: {
+  memories: memory[]
+  isPublic?: boolean
+}) => (
+  <div className="flex flex-col gap-10">
+    {memories.map((memory) => (
+      <div key={memory.id} className="space-y-4">
+        <time className="ml-[-4rem] flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
+          {dayjs(memory.createdAt).format('DD[ de ]MMMM[, ]YYYY')}
+        </time>
+        {memory.coverUrl !== '' ? (
+          <FilePreview mediaURL={memory.coverUrl} />
+        ) : null}
+        {memory.excerpt !== '' ? (
+          <p className="text-lg leading-relaxed text-gray-100">
+            {memory.excerpt}
+          </p>
+        ) : null}
+        {isPublic ? null : (
+          <Link
+            className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100"
+            href={`/memories/${memory.id}`}
+          >
+            Ler mais
+            <ArrowRight className="h-4 w-4" />
+          </Link>
+        )}
+      </div>
+    ))}
+  </div>
+)
+
 export default async function Home() {
   const isAuthenticated = cookies().has('token')
   const token = cookies().get('token')?.value
 
-  if (!isAuthenticated) return <EmptyMemories />
+  if (!isAuthenticated) {
+    const { data } = await api.get<memory[]>('/public-memories')
+
+    return <RenderMemories memories={data} isPublic />
+  }
 
   const { data } = await api.get<memory[]>('/memories', {
     headers: {
@@ -55,30 +94,5 @@ export default async function Home() {
 
   if (data.length === 0) return <EmptyMemories />
 
-  return (
-    <div className="flex flex-col gap-10">
-      {data.map((memory) => (
-        <div key={memory.id} className="space-y-4">
-          <time className="ml-[-4rem] flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
-            {dayjs(memory.createdAt).format('DD[ de ]MMMM[, ]YYYY')}
-          </time>
-          {memory.coverUrl !== '' ? (
-            <FilePreview mediaURL={memory.coverUrl} />
-          ) : null}
-          {memory.excerpt !== '' ? (
-            <p className="text-lg leading-relaxed text-gray-100">
-              {memory.excerpt}
-            </p>
-          ) : null}
-          <Link
-            className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100"
-            href={`/memories/${memory.id}`}
-          >
-            Ler mais
-            <ArrowRight className="h-4 w-4" />
-          </Link>
-        </div>
-      ))}
-    </div>
-  )
+  return <RenderMemories memories={data} />
 }
